8cb41c44d079384bd4361c6acd4d2c166abba1c3
wild_duck <wezonaser50@gmail.com>
2024-06-30 07:30:43 +0300

    chore: nothing

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,29 +1,28 @@
 import path from 'path'
-import react from '@vitejs/plugin-react'
 import { defineConfig, loadEnv } from 'vite'
-import viteReact from '@vitejs/plugin-react'
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import react from '@vitejs/plugin-react-swc'
+import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, process.cwd(), '')
-    return {
-        define: {
-            'process.env.NEXT_PUBLIC_SUPABASE_URL': JSON.stringify(env.NEXT_PUBLIC_SUPABASE_URL),
-            'process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY': JSON.stringify(env.NEXT_PUBLIC_SUPABASE_ANON_KEY),
-            'process.env.ROOT_URL': JSON.stringify(env.ROOT_URL),
-        },
-        plugins: [
-            TanStackRouterVite({
-                // experimental: {
-                //   enableCodeSplitting: true,
-                // },
-            }),
-            viteReact(),
-        ],
-        resolve: {
-            alias: {
-                '@': path.resolve(__dirname, './src'),
-            },
-        },
-    }
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    define: {
+      'process.env.NEXT_PUBLIC_SUPABASE_URL': JSON.stringify(env.NEXT_PUBLIC_SUPABASE_URL),
+      'process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY': JSON.stringify(env.NEXT_PUBLIC_SUPABASE_ANON_KEY),
+      'process.env.ROOT_URL': JSON.stringify(env.ROOT_URL),
+    },
+    plugins: [
+      TanStackRouterVite({
+        // experimental: {
+        //   enableCodeSplitting: true,
+        // },
+      }),
+      react(),
+    ],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+  }
 })
