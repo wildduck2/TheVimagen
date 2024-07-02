66994a3ac6f7f86975ed8ae76f37fae2b406f579
wild_duck <wezonaser50@gmail.com>
2024-07-02 09:53:06 +0300

    feat: refactoring code to fetach dynamicaly

---
+++ server/src/services/Email/Email.type.ts
@@ -0,0 +1,14 @@
+import { ThreadType } from 'controllers'
+
+export type GetIdsFromGmailAPIType = {
+  access_token: string
+  maxResults: number
+  distnation: string
+  fields: string
+  labelIds: string
+}
+export interface FetchEachOneWithIdType
+  extends Omit<GetIdsFromGmailAPIType, 'maxResults' | 'labelIds'> {
+  groupOfIds: ThreadType[]
+  format: string
+}
