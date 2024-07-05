74c04c194ff67d662955122f76df314fea909e0f
wild_duck <wezonaser50@gmail.com>
2024-07-05 15:47:05 +0300

    feat: using scroll restoring from tanstack router to sotre the scoll postion for pages

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -1,12 +1,13 @@
-import { createRootRoute, Outlet } from '@tanstack/react-router'
+import { createRootRoute, Outlet, ScrollRestoration } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 export const Route = createRootRoute({
-  // <TanStackRouterDevtools position="bottom-right" />
-  //     <ReactQueryDevtools initialIsOpen={false} />
   component: () => (
     <>
+      <TanStackRouterDevtools position="bottom-right" />
+      <ReactQueryDevtools initialIsOpen={false} />
+      <ScrollRestoration />
       <Outlet />
     </>
   ),
