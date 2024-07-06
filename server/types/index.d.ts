c95e06c3ba1e972eaf94c74d2b2c76557db6a1f2
wild_duck <wezonaser50@gmail.com>
2024-07-06 21:50:04 +0300

    chore: server config

--- server/types/index.d.ts
+++ server/types/index.d.ts
@@ -3,43 +3,43 @@ import { Decimal, JsonValue } from '@prisma/client/runtime/library'
 import 'express-session'
 
 declare module 'express-session' {
-    interface SessionData {
-        user: User
-        otp: string
-        oauth_user_data?: OAuthToken | null
-    }
+  interface SessionData {
+    user: User
+    otp: string
+    oauth_user_data?: OAuthToken | null
+  }
 }
 
 export interface User {
-    id: string
-    first_name?: string | null
-    last_name?: string | null
-    user_name: string
-    google_id?: string | null
-    email: string
-    address?: JsonValue | null
-    age?: number | null
-    bio?: string | null
-    picture?: string | null
-    pronounce?: string | null
-    years_of_exprience?: number | null
-    profession?: string | null
-    lastlogin_ip?: string | null
-    password: string
-    verified_email?: boolean | null
-    created_at: Date
-    updated_at: Date
+  id: string
+  first_name?: string | null
+  last_name?: string | null
+  user_name: string
+  google_id?: string | null
+  email: string
+  address?: JsonValue | null
+  age?: number | null
+  bio?: string | null
+  picture?: string | null
+  pronounce?: string | null
+  years_of_exprience?: number | null
+  profession?: string | null
+  lastlogin_ip?: string | null
+  password: string
+  verified_email?: boolean | null
+  created_at: Date
+  updated_at: Date
 }
 
 declare global {
-    interface Wildduck {
-        userName: string
-        age: number
-    }
+  interface Wildduck {
+    userName: string
+    age: number
+  }
 }
 
 declare global {
-    type Foo = {
-        ahmed: string
-    }
+  type Foo = {
+    ahmed: string
+  }
 }
