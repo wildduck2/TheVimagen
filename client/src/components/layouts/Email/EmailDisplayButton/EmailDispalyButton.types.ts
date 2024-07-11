51ccbfafd5848b86e3e50b1b050aec529cdf2b66
wild_duck <wezonaser50@gmail.com>
2024-07-11 21:57:52 +0300

    feat: workign on the mini tool bar for the email [no-ready]

--- client/src/components/layouts/Email/EmailDisplayButton/EmailDispalyButton.types.ts
+++
@@ -1,7 +0,0 @@
-import { ReactElement } from 'react'
-
-export type EmaildispalyButtonType = {
-  emailSelectedId: string[]
-  label: string
-  icon: ReactElement
-}
--- client/src/components/layouts/Email/EmailDisplayButton/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './EmailDispalyButton'
-export * from './EmailDispalyButton.types'
