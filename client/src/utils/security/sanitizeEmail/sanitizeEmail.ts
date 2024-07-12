5b7f5b6029113275fe9ad8d9c553e5fce4e6b0f9
wild_duck <wezonaser50@gmail.com>
2024-07-13 00:40:06 +0300

    feat: security sanitizeEmail

---
+++ client/src/utils/security/sanitizeEmail/sanitizeEmail.ts
@@ -0,0 +1,15 @@
+import DOMPurify from 'dompurify'
+
+export const sanitizeEmailContent = (htmlContent: string) => {
+  DOMPurify.addHook('afterSanitizeAttributes', function (node) {
+    if ('target' in node) {
+      node.setAttribute('target', '_blank')
+    }
+    if (!node.getAttribute('rel') || !node.getAttribute('rel').match(/^(noopener|noreferrer|nofollow)$/gi)) {
+      node.setAttribute('rel', 'noopener noreferrer')
+    }
+  })
+  const sanitizedContent = DOMPurify.sanitize(htmlContent)
+
+  return sanitizedContent
+}
