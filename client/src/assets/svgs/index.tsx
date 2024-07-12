d3608a682f2855d82e5a1ce0041a65bc6013134a
wild_duck <wezonaser50@gmail.com>
2024-07-13 00:39:43 +0300

    feat: refactorin and adding last things [ not ready ]

--- client/src/assets/svgs/index.tsx
+++ client/src/assets/svgs/index.tsx
@@ -2036,4 +2036,38 @@ export const Icon = {
             />
         </svg>
     ),
+
+    justify: ({ className }: IconType) => (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className={className}
+        >
+            <line
+                x1="3"
+                x2="21"
+                y1="6"
+                y2="6"
+            />
+            <line
+                x1="3"
+                x2="21"
+                y1="12"
+                y2="12"
+            />
+            <line
+                x1="3"
+                x2="21"
+                y1="18"
+                y2="18"
+            />
+        </svg>
+    ),
 }
