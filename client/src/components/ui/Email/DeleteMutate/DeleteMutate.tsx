db49aa60302f8c860d793d234eeea34da805b9a9
wild_duck <wezonaser50@gmail.com>
2024-07-14 02:43:23 +0300

    feat: DeleteMutate client

---
+++ client/src/components/ui/Email/DeleteMutate/DeleteMutate.tsx
@@ -0,0 +1,44 @@
+import { toast } from 'sonner'
+import { useMutation } from '@tanstack/react-query'
+
+import { getCookie, trashMessage } from '@/utils'
+import { Icon } from '@/assets'
+import { queryClient } from '@/main'
+import { PaginatedMessages } from '../TrashMutate'
+import { ToggleToolTipSpanWrapper } from '../..'
+import { DeleteMutateProps } from './DeleteMutate.types'
+
+export const DeleteMutate = ({ disabled, threadId, tip }: DeleteMutateProps) => {
+  const currentQueryKey = JSON.parse(getCookie('query:key')) || ['primary', { q: 'label:inbox category:primary' }]
+
+  const startMutation = useMutation({
+    mutationKey: ['delete-Message', { threadId }],
+    mutationFn: () => trashMessage({ threadId }),
+    onSuccess: () => {
+      queryClient.setQueryData<PaginatedMessages>(currentQueryKey, (oldData) => {
+        if (!oldData) return { pages: [], pageParams: [] }
+        return {
+          ...oldData,
+          pages: oldData.pages.map((page) => ({
+            ...page,
+            messages: page.messages.filter((message) => message.threadId !== threadId),
+          })),
+        }
+      })
+      toast.success(`Messages has been Deleted!`)
+    },
+  })
+  return (
+    <>
+      <ToggleToolTipSpanWrapper
+        disabled={disabled}
+        tip={tip}
+        onClick={() => {
+          startMutation.mutate()
+        }}
+      >
+        <Icon.X />
+      </ToggleToolTipSpanWrapper>
+    </>
+  )
+}
